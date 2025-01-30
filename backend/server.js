32d1d609ce53c6e9bd850e9effcea393937c9e0b
macmillanscg <developer@macmillanscg.com>
2025-01-30 15:18:59 -0500

    skdf

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,9 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));  // Serving static files from client/build
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));  // Serving the index.html from client/build
 });
 
 
